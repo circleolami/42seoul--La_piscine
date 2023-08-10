e9b77b4c2b48c85b4e2b58e288b1badf55b45bac
Gaeun Kim <gkim2@c10r1s10.42seoul.kr>
2023-08-11 04:38:52 +0900

    15. Shell01

---
+++ Shell/Shell01/ex07/r_dwssap.sh
@@ -0,0 +1,1 @@
+cat /etc/passwd | sed '/^#/d' | awk -F : 'NR % 2 == 0{print $1}' | rev |sort -r | awk -v line1=$FT_LINE1 -v line2=$FT_LINE2 'NR >= line1 && NR <= line2{printf("%s, ",$0)}' | sed 's/..$/./' | tr -d '\n'
